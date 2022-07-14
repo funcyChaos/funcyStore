97809070afca7b3b8119c71dc02b035c6eb2d10b
Austin Reilly <areilly@alphaworks.tech>
2022-07-13 22:35:18 -0700

    rearrange scss finish overlay for cats scss is okay

--- js/nav/all_cats.js
+++ js/nav/all_cats.js
@@ -1,16 +1,17 @@
 // const cats_close = document.getElementById('all_cats_close');
 const allCats = document.getElementById('all_categories');
+const catsOver = document.getElementById('cats_overlay');
 
 function cats_open(e){
 
 	e.preventDefault();
-	// allCats.style.display = 'flex';
 	allCats.style.left = 0;
+	catsOver.style.left = 0;
 }
 
 function cats_close(e){
 
 	e.preventDefault();
-	allCats.style.left = '-200px'
-	// allCats.style.display = 'none';
+	allCats.style.left = '-200px';
+	catsOver.style.left = '-100%';
 }
