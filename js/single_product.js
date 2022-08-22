f36907e5b5e74462096b8409bc1c5c6c77fe1a0b
AustinR <slidechaser@gmail.com>
2022-08-22 09:06:01 -0700

    start js development windows has no sass x.x

--- js/single_product.js
+++ js/single_product.js
@@ -1,18 +1,24 @@
 const preview2 = document.getElementById('preview_2');
 
+const nav = document.getElementById('navbar').offsetHeight
+
+const previewElement = document.getElementById('preview_element');
+
 document.addEventListener('scroll', (e)=>{
-	
-	console.log(this.scrollY);
 
 	const scroll = this.scrollY;
 	const fadeLength = 350;
 
-	if(scroll === 0){
-
-		preview2.style.opacity = 0;
-	}else{
-
-		preview2.style.opacity = scroll / fadeLength;
-		console.log(preview2.style.opacity);
-	}
+	if(scroll === 0){preview2.style.opacity = 0;}
+	else{preview2.style.opacity = scroll / fadeLength;}
 });
+
+if((window.innerHeight - (nav + 20)) > previewElement.offsetHeight){
+
+	previewElement.style.top = `${nav + 10}px`;
+}else{
+
+	console.log('triggered');
+
+	previewElement.style.top = `${previewElement.offsetHeight - window.innerHeight + nav}px`;
+}
